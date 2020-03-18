505de2c1d544c76d2598991304e64aaf43baaa0d
521liqing <18851152606@163.com>
2020-03-18 09:51:54 +0800

    将一个数组转换为字符串

---
+++ src/main/java/ArrayDemo5.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class ArrayDemo5 {
+    public static void main(String[] args) {
+        int[] nums = {25, 7, 126, 53, 14, 86};
+        System.out.println("输出数组nums中的元素：" + Arrays.toString(nums));
+    }
+}
